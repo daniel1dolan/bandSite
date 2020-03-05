a203a25cefcb911356263db9c772bd98616c50bc
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-03-05 09:12:58 -0600

    added feedback styling

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-let data = require("../data/feedback.json");
+let data = require("../data/data.json");
+let feedbackData = require("../data/feedback.json");
 
 router.get("/feedback", (req, res) => {
+  let pageAlbums = [];
+  data.albums.forEach(albumObj => {
+    pageAlbums.push(albumObj);
+  });
   res.render("feedback", {
-    pageTitle: "Feedback"
+    pageTitle: "Feedback",
+    albumInfo: pageAlbums
   });
 });
 
